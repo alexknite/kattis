c9e7ef78ea728d9fcee867fb35d4c50f73ed71cd
alexknite <alexniteo@gmail.com>
2024-04-12 23:46:21 -0700

    got nowhere...

--- cpp/delimitersoup.cpp
+++ cpp/delimitersoup.cpp
@@ -6,23 +6,24 @@ int main(){
     stack<char> s;
     int n;
     string line;
-    cin >> n >> line;
+    cin >> n;
+    getline(cin >> ws, line);
+
     for(int i=0; i<n; i++){
         char c = line[i];
         if(c == ' ') continue;
         else if(c == '(' || c == '{' || c== '['){
             s.push(c);
-        } else {
-            if((c == ')' && s.top() != '(' ) ||
+        } else if(s.empty() ||
+           ((c == ')' && s.top() != '(') ||
             (c == '}' && s.top() != '{') ||
-            (c == ']' && s.top() != '[')){
+            (c == ']' && s.top() != '[') )){
                 cout << c << " " << i << endl;
                 return 0;
             } else {
                 s.pop();
             }
         }
-    }
     cout << "okay so far" << endl;
     return 0;
 }
